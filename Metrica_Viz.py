2faf1387a57c2d8f8e53d9b24c31a51213bf6523
jdl8 <98133567+jdl8@users.noreply.github.com>
2025-04-20 19:39:25 -0500

    Update Metrica_Viz.py

--- Metrica_Viz.py
+++ Metrica_Viz.py
@@ -108,7 +108,7 @@ def plot_pitch( field_dimen = (106.0,68.0), field_color ='green', linewidth=2, m
     ax.set_axisbelow(True)
     return fig,ax
 
-def plot_frame( hometeam, awayteam, figax=None, team_colors=('r','b'), field_dimen = (106.0,68.0), include_player_velocities=False, PlayerMarkerSize=10, PlayerAlpha=0.7, annotate=False ):
+def plot_frame( hometeam, awayteam, figax=None, team_colors=('r','b'), field_dimen = (106.0,68.0), include_player_velocities=False, Playermarkersize=10, PlayerAlpha=0.7, annotate=False ):
     """ plot_frame( hometeam, awayteam )
     
     Plots a frame of Metrica tracking data (player positions and the ball) on a football pitch. All distances should be in meters.
@@ -121,7 +121,7 @@ def plot_frame( hometeam, awayteam, figax=None, team_colors=('r','b'), field_dim
         team_colors: Tuple containing the team colors of the home & away team. Default is 'r' (red, home team) and 'b' (blue away team)
         field_dimen: tuple containing the length and width of the pitch in meters. Default is (106,68)
         include_player_velocities: Boolean variable that determines whether player velocities are also plotted (as quivers). Default is False
-        PlayerMarkerSize: size of the individual player marlers. Default is 10
+        Playermarkersize: size of the individual player marlers. Default is 10
         PlayerAlpha: alpha (transparency) of player markers. Defaault is 0.7
         annotate: Boolean variable that determines with player jersey numbers are added to the plot (default is False)
         
@@ -138,7 +138,7 @@ def plot_frame( hometeam, awayteam, figax=None, team_colors=('r','b'), field_dim
     for team,color in zip( [hometeam,awayteam], team_colors) :
         x_columns = [c for c in team.keys() if c[-2:].lower()=='_x' and c!='ball_x'] # column header for player x positions
         y_columns = [c for c in team.keys() if c[-2:].lower()=='_y' and c!='ball_y'] # column header for player y positions
-        ax.plot( team[x_columns], team[y_columns], color+'o', MarkerSize=PlayerMarkerSize, alpha=PlayerAlpha ) # plot player positions
+        ax.plot( team[x_columns], team[y_columns], color+'o', markersize=Playermarkersize, alpha=PlayerAlpha ) # plot player positions
         if include_player_velocities:
             vx_columns = ['{}_vx'.format(c[:-2]) for c in x_columns] # column header for player x positions
             vy_columns = ['{}_vy'.format(c[:-2]) for c in y_columns] # column header for player y positions
@@ -146,10 +146,10 @@ def plot_frame( hometeam, awayteam, figax=None, team_colors=('r','b'), field_dim
         if annotate:
             [ ax.text( team[x]+0.5, team[y]+0.5, x.split('_')[1], fontsize=10, color=color  ) for x,y in zip(x_columns,y_columns) if not ( np.isnan(team[x]) or np.isnan(team[y]) ) ] 
     # plot ball
-    ax.plot( hometeam['ball_x'], hometeam['ball_y'], 'ko', MarkerSize=6, alpha=1.0, LineWidth=0)
+    ax.plot( hometeam['ball_x'], hometeam['ball_y'], 'ko', markersize=6, alpha=1.0, LineWidth=0)
     return fig,ax
     
-def save_match_clip(hometeam,awayteam, fpath, fname='clip_test', figax=None, frames_per_second=25, team_colors=('r','b'), field_dimen = (106.0,68.0), include_player_velocities=False, PlayerMarkerSize=10, PlayerAlpha=0.7):
+def save_match_clip(hometeam,awayteam, fpath, fname='clip_test', figax=None, frames_per_second=25, team_colors=('r','b'), field_dimen = (106.0,68.0), include_player_velocities=False, Playermarkersize=10, PlayerAlpha=0.7):
     """ save_match_clip( hometeam, awayteam, fpath )
     
     Generates a movie from Metrica tracking data, saving it in the 'fpath' directory with name 'fname'
@@ -165,7 +165,7 @@ def save_match_clip(hometeam,awayteam, fpath, fname='clip_test', figax=None, fra
         team_colors: Tuple containing the team colors of the home & away team. Default is 'r' (red, home team) and 'b' (blue away team)
         field_dimen: tuple containing the length and width of the pitch in meters. Default is (106,68)
         include_player_velocities: Boolean variable that determines whether player velocities are also plotted (as quivers). Default is False
-        PlayerMarkerSize: size of the individual player marlers. Default is 10
+        Playermarkersize: size of the individual player marlers. Default is 10
         PlayerAlpha: alpha (transparency) of player markers. Defaault is 0.7
         
     Returrns
@@ -196,7 +196,7 @@ def save_match_clip(hometeam,awayteam, fpath, fname='clip_test', figax=None, fra
             for team,color in zip( [hometeam.loc[i],awayteam.loc[i]], team_colors) :
                 x_columns = [c for c in team.keys() if c[-2:].lower()=='_x' and c!='ball_x'] # column header for player x positions
                 y_columns = [c for c in team.keys() if c[-2:].lower()=='_y' and c!='ball_y'] # column header for player y positions
-                objs, = ax.plot( team[x_columns], team[y_columns], color+'o', MarkerSize=PlayerMarkerSize, alpha=PlayerAlpha ) # plot player positions
+                objs, = ax.plot( team[x_columns], team[y_columns], color+'o', markersize=Playermarkersize, alpha=PlayerAlpha ) # plot player positions
                 figobjs.append(objs)
                 if include_player_velocities:
                     vx_columns = ['{}_vx'.format(c[:-2]) for c in x_columns] # column header for player x positions
@@ -204,7 +204,7 @@ def save_match_clip(hometeam,awayteam, fpath, fname='clip_test', figax=None, fra
                     objs = ax.quiver( team[x_columns], team[y_columns], team[vx_columns], team[vy_columns], color=color, scale_units='inches', scale=10.,width=0.0015,headlength=5,headwidth=3,alpha=PlayerAlpha)
                     figobjs.append(objs)
             # plot ball
-            objs, = ax.plot( team['ball_x'], team['ball_y'], 'ko', MarkerSize=6, alpha=1.0, LineWidth=0)
+            objs, = ax.plot( team['ball_x'], team['ball_y'], 'ko', markersize=6, alpha=1.0, LineWidth=0)
             figobjs.append(objs)
             # include match time at the top
             frame_minute =  int( team['Time [s]']/60. )
